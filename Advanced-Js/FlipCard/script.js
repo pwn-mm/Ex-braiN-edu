278d0f211cde6ed300499b809b12c96713b01fc3
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2023-08-21 14:45:55 +0630

    Documentations added

--- Advanced-Js/FlipCard/script.js
+++ Advanced-Js/FlipCard/script.js
@@ -10,7 +10,7 @@ const finalScore = document.getElementById('finalscore');
 
 let isGameClear = false;
 let isGameStart = false;
-let timeLeft = 20;
+let timeLeft = 30;
 let countdownInterval;
 
 function startGame() {
@@ -23,7 +23,12 @@ function startGame() {
   }
 }
 
-// Start Countdown timer and check if the user won.
+/**
+ * Start Countdown as soon as user clicked start
+ *
+ * Start Countdown timer and check if the user won.
+ * Game Win or Over message
+ */
 function startCountdown() {
   countdownInterval = setInterval(() => {
     timeLeft--;
@@ -37,27 +42,71 @@ function startCountdown() {
   }, 1000);
 }
 
-// Shuffle cards
-function shuffleCards() {
+/**
+ * @param {Param} array - The array to be shuffled
+ */
+/**
+ * function shuffleArray(param) {
+  for (let i = param.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [param[i], param[j]] = [param[j], param[i]]; // Swap elements
+  }
+}
+ */
+
+/**
+ * function shuffleCards() {
   card.forEach((c) => {
-    c.style = Math.floor(Math.random() * card.length);
+    console.log(c.style);
+    c.style.order = Math.floor(Math.random() * card.length);
+  });
+}
+ */
+
+/**
+ * Shuffle Card
+ */
+function shuffleCards() {
+  const cardArray = Array.from(card);
+  console.log(cardArray);
+
+  /**
+   * For any pair of elements a and b, the comparison function returns a random number between -0.5 and 0.5.
+   * If the returned value is negative (let's say -0.2), it means a should come before b in the sorted order.
+   * If the returned value is positive (let's say 0.3), it means b should come before a in the sorted order.
+   * If the returned value is 0, the order between a and b remains unchanged.
+   */
+  cardArray.sort(() => Math.random() - 0.5); // Shuffle array using sort
+
+  // Put shuffled elements back
+  cardArray.forEach((c) => {
+    container.appendChild(c);
   });
 }
 
-// Click each card
+/**
+ * Click each card
+ *
+ * Click တာနဲ့ 'show' class ကိုခေါ်မယ် ပြီးတာနဲ့ပြန်ပျောက်မယ်
+ * User က start button နှိပ်ပြီးမှသာ button click လို့ရမယ်
+ */
 function clickEachCard() {
   for (let i = 0; i < card.length; i++) {
     front[i].classList.add('show');
 
-    setInterval(() => {
+    // စစချင်းပေါ်ပြီး ပြန်ပျောက်ဖို့
+    setTimeout(() => {
       front[i].classList.remove('show');
     }, 2000);
 
     card[i].addEventListener('click', () => {
+      // User can click each card only if the game starts
       if (isGameStart) {
+        // Call 'flip' class and save 2 flips
         front[i].classList.add('flip');
         const filppedCard = document.querySelectorAll('.flip');
 
+        // if user clicks 2 flips, check if the cards match
         if (filppedCard.length == 2) {
           checkIfMatch(filppedCard[0], filppedCard[1]);
         }
@@ -66,7 +115,19 @@ function clickEachCard() {
   }
 }
 
-// Check if card1 and card2
+/**
+ *
+ * @param {*} cardOne User flip card one
+ * @param {*} cardTwo User flip card two
+ *
+ * icon သုံးထားတဲ့အတွက် icon ကို string ပြန်ပြောင်းပြီးတူလား တိုက်စစ်
+ * တူတယ်ဆိုရင် score ၁ပေါင်း
+ * ပြီးရင် 'flip' class ပြန်ဖြုတ်ပြီး 'match' ဖြစ်တဲ့အတိုင်းထား
+ *
+ * 'match' class တွေအကုန်စုလိုက်ပြီး match and card length တွေတူတယ်ဆိုရင် game clear
+ * မတူဘူးဆိုရင် ပြန်လှန်မယ်
+ */
+
 function checkIfMatch(cardOne, cardTwo) {
   console.log(cardOne, cardTwo);
   const iconOne = cardOne.querySelector('.front i').classList;
@@ -94,7 +155,10 @@ function checkIfMatch(cardOne, cardTwo) {
   }
 }
 
-// Handle Game Over function
+/**
+ *
+ * @param {*} param Game Over (or) Game Win
+ */
 function handleGameOver(param) {
   finalScoreContainer.style.display = 'block';
   finalScore.innerHTML = parseInt(score.innerHTML);
---
+++ JS-interviews/q1.js
@@ -0,0 +1,84 @@
+//Test Case 1
+let allexam = [
+  [{ classid: 1, rank: 3 }],
+  [{ classid: 1, rank: 5 }],
+  [{ classid: 2, rank: 1 }],
+  [{ classid: 2, rank: 6 }],
+  [{ classid: 3, rank: 1 }],
+  [{ classid: 'A', rank: 1 }],
+  [{ classid: 'A', rank: 1 }],
+  [{ classid: 'a', rank: 1 }],
+];
+
+//Expected Output
+[
+  [
+    { classid: 1, rank: 3 },
+    { classid: 1, rank: 5 },
+  ],
+  [
+    { classid: 2, rank: 1 },
+    { classid: 2, rank: 6 },
+  ],
+  [{ classid: 3, rank: 1 }],
+];
+
+//TestCase 2
+let allexam1 = [
+  [{ classid: 1, rank: 3 }],
+  [{ classid: 2, rank: 1 }],
+  [{ classid: 3, rank: 1 }],
+  [{ classid: 3, rank: 3 }],
+  [{ classid: 3, rank: 2 }],
+];
+
+// funcition
+//Expected Output
+[
+  [{ classid: 1, rank: 3 }],
+  [{ classid: 2, rank: 1 }],
+  [
+    { classid: 3, rank: 1 },
+    { classid: 3, rank: 3 },
+    { classid: 3, rank: 2 },
+  ],
+];
+
+let groupedArrayData = [];
+let currentGroup = [];
+let currentClassID = null;
+
+/**
+ *
+ * @param {*} param is used to accept any kinds of array
+ */
+function groupArray(param) {
+  for (let i = 0; i < param.length; i++) {
+    //တစ်ခန်းစီထုတ်ပြီး ထည့်ထား
+    let eachExam = param[i][0];
+
+    // group လုပ်ထားတာမရှိရင် or လက်ရှိ group ထဲက id နဲ့ exam ထဲက id နဲ့တူရင် စုမယ်
+    if (
+      currentGroup.length == 0 ||
+      currentGroup[0].classid == eachExam.classid
+    ) {
+      // Group အသစ်ထဲထည့်တယ်
+      currentGroup.push(eachExam);
+    } else {
+      // id မတူတဲ့ new array ကိုတွေ့တာ
+      groupedArrayData.push(currentGroup);
+      // group အသစ်စမယ်
+      currentGroup = [];
+      currentGroup.push(eachExam);
+    }
+  }
+
+  // နောက်ဆုံးအကုန်ပြီးတာတောင် group ထဲမှာ data ကျန်သေးတယ်ဆိုရင် groupedArrayData ထဲထည့်ပေးလိုက်မယ်
+  if (currentGroup.length > 0) {
+    groupedArrayData.push(currentGroup);
+  }
+
+  console.log(groupedArrayData);
+}
+
+groupArray(allexam);
