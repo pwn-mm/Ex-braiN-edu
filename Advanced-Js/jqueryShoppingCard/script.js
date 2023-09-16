2bbc3b0a19afb5cceb96eb2820022e8a4dc51601
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2023-09-16 21:18:08 +0630

    Jquery online shop code updated | new functions added

--- Advanced-Js/jqueryShoppingCard/script.js
+++ Advanced-Js/jqueryShoppingCard/script.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
-  // Hide the cart initially
-  $('.cart').hide();
-  $('#discounttitle').hide();
+  const $cart = $('.cart');
+  const $grandTotal = $('#grand');
+  const $discountTitle = $('#discounttitle');
+  const $products = $('.products');
+  const $calculateItem = $('.calculateitem');
+  const $delivery = $('#delivery');
 
   var selectedItemValue, itemName, itemCode, grandTotal;
   var selectedDeliveryValue = 0;
   var isCartEmpty = false;
   var selectedItems = [];
 
+  // Hide the cart initially
+  $cart.hide();
+  $discountTitle.hide();
+
   //   Show card function
   let showCart = () => {
     // If user selects card, slide it in, slide out if there is not item left in the cart
     if (!isCartEmpty) {
-      $('.cart').slideDown('slow');
+      $cart.slideDown('slow');
       cartOpen = true;
     }
   };
@@ -20,13 +27,14 @@ $(document).ready(function () {
   // Calculate grand total value
   let calculateGrandTotalValue = () => {
     grandTotal =
-      selectedItems?.reduce(function (acc, value) {
-        return acc + value;
+      selectedItems?.reduce(function (acc, item) {
+        console.log(item);
+        return acc + item.selectedItemValue * item.selectedQuantity;
       }, 0) + selectedDeliveryValue;
     console.log(grandTotal);
 
     // Update the grand total display
-    $('#grand').text(grandTotal + ' Ks');
+    $grandTotal.text(grandTotal + ' Ks');
   };
 
   // Calculate user selected card's value
@@ -36,50 +44,80 @@ $(document).ready(function () {
     selectedItemValue = parseFloat(
       clickedItem.find('.price').text().replace('Ks ', '')
     );
-    selectedItems.push(selectedItemValue);
+
+    // Check if the item is already selected
+    if (selectedItems.some((item) => item.itemCode === itemCode)) {
+      // Show an alert if the item has already been selected
+      alert('You have already selected this item.');
+      return null;
+    }
+
+    selectedItems.push({ itemCode, selectedItemValue, selectedQuantity: 1 });
     return selectedItemValue;
     // console.log(selectedItemValue);
   };
 
   // Show the cart when user clicks any card
-  $('.products').on('click', '.card', function () {
+  $products.on('click', '.card', function () {
     showCart();
     // Calculate the selected item's value
-    getUserSelectedCardValues($(this));
+    var userSelectedItemValue = getUserSelectedCardValues($(this));
 
-    // selectedItems.push({
-    //   name: itemName,
-    //   code: itemCode,
-    //   price: selectedItemValue,
-    // });
+    if (userSelectedItemValue !== null) {
+      // Create and append a new userCalculateItem element
+      var newUserItem = $('<div class="userCalculateItem">');
+      newUserItem.html(`
+      <img src="${$(this).find('img').attr('src')}" alt="shirt" width="70px" />
+      <div><p>${itemName}</p><p>${itemCode}</p></div>
+      <input type="text" name="item" value="1" />
+      <p><ion-icon name="trash"></ion-icon></p>
+    `);
 
-    // Create and append a new userCalculateItem element
-    var newUserItem = $('<div class="userCalculateItem">');
-    newUserItem.append(
-      '<img src="' +
-        $(this).find('img').attr('src') +
-        '" alt="shirt" width="70px" />'
-    );
-    newUserItem.append(
-      '<div><p>' + itemName + '</p><p>' + itemCode + '</p></div>'
-    );
-    newUserItem.append('<input type="text" name="item" id="item" />');
-    newUserItem.append('<p><ion-icon name="trash"></ion-icon></p>');
+      // Get the current itemCode value for user input quantity
+      var currentItemCode = itemCode;
+      console.log(currentItemCode);
 
-    $('.calculateitem').append(newUserItem);
+      $calculateItem.append(newUserItem);
 
-    // Detect change in delivery values and update
-    $('#delivery').change(function () {
-      selectedDeliveryValue = parseFloat($(this).val());
+      // 1 and 9 ကြားအတွင်းစစ်
+      var previousQuantity = 1;
 
-      // Recalculate the grand total
+      // Detect changes in input box whether if the user changes the quantity
+      $calculateItem.on('change', 'input[name="item"]', function () {
+        // Get the updated quantity and item code
+        var updatedQuantity = parseInt($(this).val());
+        console.log(updatedQuantity);
+
+        if (updatedQuantity < 1 || updatedQuantity > 9) {
+          alert('Please enter a quantity between 1 and 9.');
+          $(this).val(previousQuantity);
+        } else {
+          // Update the quantity and item value
+          // User input ပြင်မယ့် item code နဲ့ ရှိပြီးသားထဲက item code နဲ့တူလားတိုက်စစ် / တူရင် selectedQuantity ကို update လုပ်
+          selectedItems.forEach(function (item) {
+            if (item.itemCode == currentItemCode) {
+              item.selectedQuantity = updatedQuantity;
+              previousQuantity = updatedQuantity;
+            }
+          });
+          // Recalculate the grand total
+          calculateGrandTotalValue();
+        }
+      });
+
+      // Detect change in delivery values and update
+      $delivery.change(function () {
+        selectedDeliveryValue = parseFloat($(this).val());
+
+        // Recalculate the grand total
+        calculateGrandTotalValue();
+      });
+
+      // Get the initial selected delivery option value
+      selectedDeliveryValue = parseFloat($delivery.val());
+
+      // Calculate the grand total initially
       calculateGrandTotalValue();
-    });
-
-    // Get the initial selected delivery option value
-    selectedDeliveryValue = parseFloat($('#delivery').val());
-
-    // Calculate the grand total initially
-    calculateGrandTotalValue();
+    }
   });
 });
